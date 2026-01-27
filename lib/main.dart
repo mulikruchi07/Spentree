505d8229bf7ee7be6504b951cee237c33a246143
Ruchi Mulik <mulikruchi07@gmail.com>
2026-01-28 02:58:20 +0530

    feat: directory strcture, main point to initial page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'screens/onboarding/onboarding_screen.dart'; // Import the file
 
 void main() {
   runApp(const MyApp());
@@ -11,99 +10,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SmsScreen(),
-    );
-  }
-}
-
-class SmsScreen extends StatefulWidget {
-  const SmsScreen({super.key});
-
-  @override
-  State<SmsScreen> createState() => _SmsScreenState();
-}
-
-class _SmsScreenState extends State<SmsScreen> {
-  static const MethodChannel _channel = MethodChannel('sms_channel');
-
-  List<dynamic> smsList = [];
-  bool loading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    initSmsFlow();
-  }
-
-  Future<void> initSmsFlow() async {
-    await requestSmsPermission();
-    await fetchSms();
-  }
-
-  Future<void> requestSmsPermission() async {
-    var status = await Permission.sms.status;
-
-    if (!status.isGranted) {
-      await Permission.sms.request();
-    }
-  }
-
-  Future<void> fetchSms() async {
-    try {
-      final List<dynamic> result = await _channel.invokeMethod('getAllSms');
-
-      setState(() {
-        smsList = result;
-        loading = false;
-      });
-    } catch (e) {
-      setState(() {
-        loading = false;
-      });
-      debugPrint("Error fetching SMS: $e");
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('SMS Fetch Test')),
-      body: loading
-          ? const Center(child: CircularProgressIndicator())
-          : smsList.isEmpty
-          ? const Center(child: Text('No SMS found'))
-          : ListView.builder(
-              itemCount: smsList.length,
-              itemBuilder: (context, index) {
-                final sms = smsList[index];
-
-                return Card(
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 6,
-                  ),
-                  child: ListTile(
-                    title: Text(
-                      sms['address'] ?? '',
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text(
-                      sms['body'] ?? '',
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    trailing: Text(
-                      DateTime.fromMillisecondsSinceEpoch(
-                        sms['date'],
-                      ).toLocal().toString().split('.')[0],
-                      style: const TextStyle(fontSize: 10),
-                    ),
-                  ),
-                );
-              },
-            ),
+      title: 'SpenTree',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const OnboardingScreen(),
     );
   }
 }

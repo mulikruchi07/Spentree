0e334aac43586c1a4c62f6edbc15c5098253b62f
Ruchi Mulik <mulikruchi07@gmail.com>
2026-02-02 02:13:15 +0530

    android auth permit

--- android/app/src/main/kotlin/com/example/spentree/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/spentree/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.example.spentree
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity() {
+// We inherit from FlutterFragmentActivity to support biometrics
+class MainActivity: FlutterFragmentActivity() {
 
     private val CHANNEL = "sms_channel"
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
 
+        // Integrating your SMS Reader MethodChannel
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
             .setMethodCallHandler { call, result ->
                 if (call.method == "getAllSms") {
@@ -30,4 +34,9 @@ class MainActivity : FlutterActivity() {
                 }
             }
     }
+    override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    // This line prevents screenshots and blurs the app in the task switcher
+    window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+}
 }
